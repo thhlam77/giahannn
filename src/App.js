79073d8c9b29f76bc1fa98aecdecee4736d8c766
Lin <102591122+thhlam77@users.noreply.github.com>
2025-03-17 21:59:28 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -83,7 +83,7 @@ const Title = styled(motion.h1)`
   transition={{ duration: 1 }}
   className="responsive-title"
 >
- Chúc mừng ngày 8/3, cún iu cụa anhhh!
+ Chúi iuu của mình oiiii!
 </Title>
 
 const PuppyContainer = styled(motion.div)`
@@ -205,7 +205,7 @@ function App() {
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 1 }}
       >
-        Chúc mừng ngày 8/3, cún iu cụa anhhh!
+        Chúi iuu của mình oiiii!
       </Title>
       
       <PuppyContainer
